4e98a2dc6fea8a2d827de7cf37c16b6a79864ef3
EcaCosca <enriquecoscarelli@gmail.com>
2023-08-17 13:36:30 +0200

    done ex 2

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const path = require('path')
 
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res)=>{
     res.send("Working")
 })
+
+app.put('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'pages/index.html'))
+})
+
+
+
 
 app.listen(port,()=>{
   console.log(`http://localhost:${port}/`);
