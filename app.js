f6f5c5c52f3035088633bb437b9cb68674691a31
EcaCosca <enriquecoscarelli@gmail.com>
2023-08-17 13:48:16 +0200

    done with ex 4

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ const path = require('path')
 
 const port = process.env.PORT || 3000
 
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.route('/')
 .get((req, res)=>{res.send("Working")})
 .put((req, res)=>{res.sendFile(path.join(__dirname, 'pages/index.html'))})
 .delete((req, res)=>{res.json({good : "yep"})})
+
+app.get('/test-ejs', (req, res)=>{
+    res.render('./index.ejs', { myTitle: 'my first title' })
+})
 
 app.listen(port,()=>{
   console.log(`http://localhost:${port}/`);
